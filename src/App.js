957d8f2c2db131fb198bcb0f0c4075b5e14873fb
Diego Rocha Chagas <drochachagas@gmail.com>
2019-04-03 13:05:58 -0300

    Create CommentDetail and ApprovalCard components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import faker from "faker";
+import ApprovalCard from "./ApprovalCard";
+import CommentDetail from "./CommentDetail";
 
 class App extends Component {
   render() {
+    const comments = [
+      "Great blog post!",
+      "Informative. Thanks",
+      "Looks great",
+      "Great comment that is an example to test if the page works well"
+    ];
+    const date = JSON.stringify(faker.date.past());
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {comments.map((content, index) => {
+          return (
+            <ApprovalCard key={`comment${index}`}>
+              <CommentDetail
+                avatarURL={faker.image.avatar()}
+                userName={faker.name.firstName()}
+                date={date}
+                content={content}
+              />
+            </ApprovalCard>
+          );
+        })}
       </div>
     );
   }
