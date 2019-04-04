2b9b0ff0959ee64fa474c1d308850f675fe34c29
Diego Rocha Chagas <drochachagas@gmail.com>
2019-04-04 09:50:14 -0300

    Build deploy

---
+++ precache-manifest.8f73ba931573fb00a738f72d1ca0e24a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca765789240f27a58a1d",
+    "url": "/comment-list/static/js/runtime~main.9411274d.js"
+  },
+  {
+    "revision": "8682eeff708e5fad6d87",
+    "url": "/comment-list/static/js/main.7b203bb4.chunk.js"
+  },
+  {
+    "revision": "94615ef4d83f719e2dd0",
+    "url": "/comment-list/static/js/2.1f0f7a07.chunk.js"
+  },
+  {
+    "revision": "8682eeff708e5fad6d87",
+    "url": "/comment-list/static/css/main.3cbf6f2a.chunk.css"
+  },
+  {
+    "revision": "ecdb9401174349adb5b69ba179cb5008",
+    "url": "/comment-list/index.html"
+  }
+];
